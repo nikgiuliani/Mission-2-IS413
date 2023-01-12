4a083132ab94c7bfd9146e1d5d9a968924ae6ee5
Nik Giuliani <94720638+nikgiuliani@users.noreply.github.com>
2023-01-12 12:42:13 -0700

    Add files via upload

---
+++ Mission-2/Mission-2/DiceRoller.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace Mission_2
+{
+    public class DiceRoller
+    {
+        Dictionary<int, int> numbersRolled = new Dictionary<int, int>();
+        public double totalNumberOfRolls;
+        public DiceRoller(int numRolls)
+        {
+            for (int i = 2; i < 13; i++)
+            {
+                numbersRolled.Add(i, 0);
+                totalNumberOfRolls = numRolls;
+            }
+        }
+        public void RollDice(int number)
+        {
+            int chosenKey = 0;
+            foreach(KeyValuePair<int, int> e in numbersRolled)
+            {
+                if (e.Key == number)
+                {
+                    chosenKey = number;
+                    break;
+                }
+            }
+            numbersRolled[chosenKey]++;
+        }
+        public ArrayList CalculateAndReturnPercentages()
+        {
+            string valueToBeAdded;
+            ArrayList finalTallies = new ArrayList();
+            foreach (KeyValuePair<int, int> e in numbersRolled)
+            {
+                valueToBeAdded = e.Key + ": " + CalculateStarsReturned(e.Value);
+                finalTallies.Add(valueToBeAdded);
+            }
+            return finalTallies;
+        }
+        public string CalculateStarsReturned(double numberOfCurrentRolls)
+        {
+            string returnString = "";
+            double percentage = (numberOfCurrentRolls / totalNumberOfRolls) * 100;
+            for (int i = 0; i < Math.Floor(percentage); i++)
+            {
+                returnString += '*';
+            }
+            return returnString;
+        }
+    }
+}
---
+++ Mission-2/Mission-2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+
+namespace Mission_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the dice throwing simulator!");
+            int numRolls = UserRollNumberPrompt();
+            DiceRoller diceRoller = new DiceRoller(numRolls);
+
+            for (int i = 0; i < numRolls; i++)
+            {
+                Random rnd = new Random();
+                int rndDiceNum = rnd.Next(2, 13);
+                diceRoller.RollDice(rndDiceNum);
+            }
+
+            Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+            Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+            Console.WriteLine($"Total number of rolls = { diceRoller.totalNumberOfRolls }.");
+
+            ArrayList returnPerc = diceRoller.CalculateAndReturnPercentages();
+            foreach(string item in returnPerc)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+        }
+        private static int UserRollNumberPrompt()
+        {
+            bool validUserInput = true;
+            int numRolls = 0;
+            do
+            {
+                Console.Write("How many dice rolls would you like to simulate? ");
+                string rollsInput = Console.ReadLine();
+                try
+                {
+                    numRolls = Int32.Parse(rollsInput);
+                    validUserInput = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please Enter a Valid Number");
+                    validUserInput = false;
+                }
+            } while (!validUserInput);
+            return numRolls;
+        }
+    }
+}
